f5a294d78556913f4bdce0b96cef44ca0de98024
USER <eugenemabansag8@gmail.com>
2026-05-04 18:45:03 +0800

    Added pseudocode

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# calculator app using inheritance
+
+## create parent classes
+
+### parent class for addition
+#### main method ask for 2 arguments and adds them
+
+### parent class for subtraction
+#### main method ask for 2 arguments and subtracts them
+
+### parent class  for multiplication
+#### main method ask for 2 arguments and multiplies them
+
+### parent class for division
+#### main method ask for 2 arguments and divides them
+
+### child class to handle user input
+#### user input to ask for 2 numbers
+#### user input to ask for operation to be used
+#### returns answer
+
+### call child class
